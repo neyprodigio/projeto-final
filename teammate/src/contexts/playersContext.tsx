7dabab7e9468f16f0133b8f4b0cf8c84f3623397
Neilson Oliveira <106606239+neyprodigio@users.noreply.github.com>
2022-11-03 21:33:37 -0300

    feat: pages and structs

---
+++ teammate/src/contexts/playersContext.tsx
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react"
+
+export const playerContext = () => {
+    const playerProvider = createContext({})
+    const {players, setPlayers} = useState()
+    return(
+        
+    )
+}
